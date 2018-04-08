493e4179e4d72f5a045a283557bdbd7cd94d5c91
DoubleJump <gareth.battensby@gmail.com>
2018-04-08 12:56:34 +0100

    Started on guide

--- demo/js/app.js
+++ demo/js/app.js
@@ -226,26 +226,20 @@ function update_spinner(dt)
 {
 	var spinner = app.spinner;
 
+	// update the spinner scale which is controlled
+	// by the GSAP animations
 	spinner.scale.setScalar(app.spin_scale);
 
 	if(key_down(Keys.MOUSE_LEFT) &! spinner.spinning)
-	{
 		spinner.spinning = true;	
-	}
-	
-	if(key_up(Keys.MOUSE_LEFT))
-	{
-		if(spinner.spinning)
-		{
-			spinner.velocity = input.mouse.delta.x * dt;
-		}
-		spinner.spinning = false;
-	}
 
+	if(key_up(Keys.MOUSE_LEFT))
+		spinner.spinning = false;
+
+	// If we are dragging set the spin velocity
+	// to amount og horizontal mouse movement
 	if(spinner.spinning)
-	{
 		spinner.velocity = input.mouse.delta.x * dt;
-	}
 
 	// Stop the spinner spinning too fast
 	spinner.velocity = clamp(spinner.velocity, -0.5,0.5);
@@ -312,6 +306,87 @@ function on_colour_click(e)
 	if(app.current_colour === colour) return;
 	app.current_colour = colour;
 
+	var ui = app.ui;
+	var casing = app.headphones.children[0];
+	var speakers = app.headphones.children[1]; 
+	var bg;
+	var bg_text;
+	var label;
+	var label_text;
+	var burger;
+	var gradient_a;
+	var gradient_b;
+
+	switch(colour)
+	{
+		case 'ultraviolet':
+		{
+			matcap = app.assets.textures.matcap_violet;
+			bg = '#E9E7F7';
+			bg_text = '#BFA4FA';
+			label = '#00A4FF';
+			label_text = '#625DA0';
+			burger = '#261493';
+			gradient_a =  '#d25afc';
+			gradient_b = '#2700bd';
+			break;
+		}
+		case 'hotpink':
+		{
+			matcap = app.assets.textures.matcap_pink;
+			bg = '#f7e7f4';
+			bg_text = '#ccafc6';
+			label = '#e20aa4';
+			label_text = '#a37e97';
+			burger = '#931477';
+			gradient_a = '#D2C900';
+			gradient_b = '#EA00BD';
+			break;
+		}
+		case 'solarflare':
+		{
+			matcap = app.assets.textures.matcap_yellow;
+			bg = '#FAFCFA';
+			bg_text = '#E8E39C';
+			label = '#FF5500';
+			label_text = '#FFC715';
+			burger = '#D8860E';
+			gradient_a = '#F7495E';
+			gradient_b = '#F7FF00';
+			break;
+		}
+		case 'midnight':
+		{
+			matcap = app.assets.textures.matcap_black;
+			bg = '#EDEDED';
+			bg_text = '#C9C6CC';
+			label = '#000000';
+			label_text = '#2F2F33';
+			burger = '#2F2F33';
+			gradient_a = '#393B42';
+			gradient_b = '#0F1112';
+			break;
+		}
+	}
+
+	ui.bg_text.innerHTML = colour;
+	casing.material.uniforms.matcap.value = matcap;
+	speakers.material.uniforms.matcap.value = matcap;
+
+	ui.bg.style.background = bg;
+	ui.bg_text.style.color = bg_text;
+	ui.label.style.fill = label;
+	ui.colour_label.style.color = label_text;
+
+	for(var i = 0; i < ui.burger_lines.length; ++i)
+	{
+		ui.burger_lines[i].style.background = burger;
+	}
+
+	var stops = ui.bg_angle.querySelectorAll('stop');
+	stops[0].setAttribute('stop-color', gradient_a);
+	stops[1].setAttribute('stop-color', gradient_b);
+
 	for(var i = 0; i < app.ui.colours.length; ++i)
 	{
 		app.ui.colours[i].classList.remove('active');
@@ -319,106 +394,4 @@ function on_colour_click(e)
 	e.target.classList.add('active');
 
 	app.animations.colour_switch.restart();
-
-	var ui = app.ui;
-	var casing = app.headphones.children[0];
-	var speakers = app.headphones.children[1]; 
-
-	//TODO put colours in a data structure and use enums
-
-	switch(colour)
-	{
-		case 'ultraviolet':
-		{
-			var matcap = app.assets.textures.matcap_violet;
-			ui.bg.style.background = '#E9E7F7';
-			ui.bg_text.style.color = '#BFA4FA';
-			ui.bg_text.innerHTML = 'ultraviolet';
-			casing.material.uniforms.matcap.value = matcap;
-			speakers.material.uniforms.matcap.value = matcap;
-			ui.label.style.fill = '#00A4FF';
-			ui.colour_label.style.color = '#625DA0';
-
-			for(var i = 0; i < ui.burger_lines.length; ++i)
-			{
-				ui.burger_lines[i].style.background = '#261493';
-			}
-
-			//set background gradient
-			var stops = ui.bg_angle.querySelectorAll('stop');
-			stops[0].setAttribute('stop-color', '#d25afc');
-			stops[1].setAttribute('stop-color', '#2700bd');
-
-			break;
-		}
-		case 'hotpink':
-		{
-			var matcap = app.assets.textures.matcap_pink;
-			ui.bg.style.background = '#f7e7f4';
-			ui.bg_text.style.color = '#ccafc6';
-			ui.bg_text.innerHTML = 'hotpink';
-			casing.material.uniforms.matcap.value = matcap;
-			speakers.material.uniforms.matcap.value = matcap;
-			ui.label.style.fill = '#e20aa4';
-			ui.colour_label.style.color = '#a37e97';
-
-			for(var i = 0; i < ui.burger_lines.length; ++i)
-			{
-				ui.burger_lines[i].style.background = '#931477';
-			}
-
-			//set background gradient
-			var stops = ui.bg_angle.querySelectorAll('stop');
-			stops[0].setAttribute('stop-color', '#D2C900');
-			stops[1].setAttribute('stop-color', '#EA00BD');
-
-			break;
-		}
-		case 'solarflare':
-		{
-			var matcap = app.assets.textures.matcap_yellow;
-			ui.bg.style.background = '#FAFCFA';
-			ui.bg_text.style.color = '#E8E39C';
-			ui.bg_text.innerHTML = 'solarflare';
-			casing.material.uniforms.matcap.value = matcap;
-			speakers.material.uniforms.matcap.value = matcap;
-			ui.label.style.fill = '#FF5500';
-			ui.colour_label.style.color = '#FFC715';
-
-			for(var i = 0; i < ui.burger_lines.length; ++i)
-			{
-				ui.burger_lines[i].style.background = '#D8860E';
-			}
-
-			//set background gradient
-			var stops = ui.bg_angle.querySelectorAll('stop');
-			stops[0].setAttribute('stop-color', '#F7495E');
-			stops[1].setAttribute('stop-color', '#F7FF00');
-
-			break;
-		}
-		case 'midnight':
-		{
-			var matcap = app.assets.textures.matcap_black;
-			ui.bg.style.background = '#EDEDED';
-			ui.bg_text.style.color = '#C9C6CC';
-			ui.bg_text.innerHTML = 'midnight';
-			casing.material.uniforms.matcap.value = matcap;
-			speakers.material.uniforms.matcap.value = matcap;
-			ui.label.style.fill = '#000000';
-			ui.colour_label.style.color = '#2F2F33';
-
-			for(var i = 0; i < ui.burger_lines.length; ++i)
-			{
-				ui.burger_lines[i].style.background = '#2F2F33';
-			}
-
-			//set background gradient
-			var stops = ui.bg_angle.querySelectorAll('stop');
-			stops[0].setAttribute('stop-color', '#393B42');
-			stops[1].setAttribute('stop-color', '#0F1112');
-
-			break;
-		}
-	}
 }
